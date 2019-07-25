b3ef8c1755276810a1b5efe3a975d6a7ba05e41b
Sami Karkinen <sami.karkinen@kavokerr.com>
2019-07-25 11:37:23 +0300

    Added working client to connect to the server

--- src/client.cpp
+++ src/client.cpp
@@ -1,8 +1,23 @@
 #include <Poco/Net/StreamSocket.h>
 #include <iostream>
 
-int main(int argc, char const *argv[])
-{
-    std::cout << "Fak yeeh!!1" << std::endl;
-    return 0;
+int main(int argc, char const *argv[]) {
+  Poco::Net::SocketAddress address("127.0.0.1:10000");
+  Poco::Net::StreamSocket connection;
+
+  // Defining the same buffer sizes as in server
+  Poco::FIFOBuffer buffer(std::size_t(1024));
+  Poco::Buffer<char> text(std::size_t(1024));
+
+  text.assign("Hello from client!", 1024);
+
+  buffer.write(text);
+
+  connection.connect(address);
+
+  connection.sendBytes(buffer);
+
+  connection.close();
+  
+  return 0;
 }
--- src/server.cpp
+++ src/server.cpp
@@ -1,25 +1,37 @@
-#include <Poco/Net/ServerSocket.h>
 #include <Poco/BasicEvent.h>
+#include <Poco/Net/ServerSocket.h>
 #include <iostream>
 
-int main(int argc, char const *argv[])
-{
-    Poco::Net::SocketAddress address("127.0.0.1:10000");
-    Poco::Net::ServerSocket server(address);
-    Poco::Net::StreamSocket connection;
-    Poco::FIFOBuffer buffer(std::size_t(1024));
-    Poco::Buffer<char> text(std::size_t(1024));
-    while (true){
-        connection = server.acceptConnection();
-        while (connection.impl()->initialized()){
-            connection.receiveBytes(buffer);
-            if (buffer.isReadable())
-            {
-                buffer.read(text);
-                std::cout << text.begin() << std::endl;
-            }
-        }
+int main(int argc, char const *argv[]) {
+  // Defining all the variables used in the connection
+  Poco::Net::SocketAddress address("127.0.0.1:10000");
+  Poco::Net::ServerSocket server(address);  // NOTE: The address is given here
+  Poco::Net::StreamSocket connection;
+
+  // Buffer size: 1024. Just some I checked online.
+  Poco::FIFOBuffer buffer(std::size_t(1024));
+  // The same size for the text, because... dunno.
+  Poco::Buffer<char> text(std::size_t(1024));
+
+  Poco::Timespan timeout_time(10, 0);
+
+  // Starting to listen for connections
+  while (true) {
+    // Accepting connections to the server
+    connection = server.acceptConnection();
+    while (connection.impl()->initialized()) {
+      // Receiving data from the connection
+      connection.receiveBytes(buffer);
+      if (buffer.isReadable()) {
+        // If there's something in the buffer -> read the buffer
+        buffer.read(text);
+        std::cout << "MESSAGE FROM CLIENT: " << text.begin() << std::endl;
+      } else {
+        // Else, close the connection and go to the acceptConnection loop
+        std::cout << "No buffer left, closing connection..." << std::endl;
+        connection.close();
+      }
     }
-    // std::cout << "Fak yeeh!!1" << std::endl;
-    return 0;
+  }
+  return 0;
 }
