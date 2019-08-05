ef28aec9a42ceed13282455a6d9f48f697de6c23
Sami Karkinen <sami.karkinen@kavokerr.com>
2019-08-05 10:47:35 +0300

    Wrote start and stop functions for client class, renamed cv_server.cpp -> cv_server_receive.cpp

--- include/cv_client.hpp
+++ include/cv_client.hpp
@@ -6,13 +6,23 @@
 #include <string>
 
 class CVSocketClient : Poco::Runnable {
+ public:
+  enum ClientState { kClientIdle, kClientSending, kClientReceiving };
+  CVSocketClient(std::string full_address_string);
+  ~CVSocketClient();
+  void StartSending(const int &width,const int &height);
+  void StartReceiving();
+  void Stop();
+  void run();
+
  private:
   Poco::Net::SocketAddress address_;
   Poco::Net::StreamSocket connection_;
   Poco::Semaphore command_sema_;
+  cv::Mat received_image_;
+  int image_width_;
+  int image_height_;
+  ClientState state_;
 
- public:
-  CVSocketClient(std::string full_address_string);
-  ~CVSocketClient();
-  void run();
+  void StateMachine();
 };
--- src/cv_client.cpp
+++ src/cv_client.cpp
@@ -1,8 +1,60 @@
 #include "cv_client.hpp"
+#include <iostream>
 
 CVSocketClient::CVSocketClient(std::string full_address_string)
-    : address_(full_address_string), connection_(address_) {}
+    : address_(full_address_string), command_sema_(1), state_(kClientIdle) {}
 
-CVSocketClient::~CVSocketClient(){
+CVSocketClient::~CVSocketClient() {
+  if (connection_.impl()->initialized()) {
     connection_.close();
+  }
+}
+
+void CVSocketClient::StartSending(const int &width, const int &height) {
+  connection_.connect(address_);
+  image_width_ = width;
+  image_height_ = height;
+  connection_.sendBytes(&image_width_, sizeof(image_width_));
+  connection_.sendBytes(&image_height_, sizeof(image_height_));
+  std::cout << "Sent image size: " << image_width_ << "x" << image_height_
+            << std::endl;
+  state_ = kClientSending;
+  command_sema_.set();
+}
+
+void CVSocketClient::StartReceiving() {
+  connection_.connect(address_);
+  connection_.receiveBytes(&image_width_, sizeof(image_width_));
+  connection_.receiveBytes(&image_height_, sizeof(image_height_));
+  std::cout << "Received image size: " << image_width_ << "x" << image_height_
+            << std::endl;
+  received_image_ =
+      cv::Mat::zeros(cv::Size2i(image_width_, image_height_), CV_8UC3);
+  state_ = kClientReceiving;
+  command_sema_.set();
+}
+
+void CVSocketClient::Stop() {
+  connection_.close();
+  state_ = kClientIdle;
+}
+
+void CVSocketClient::StateMachine() {
+  switch (state_) {
+    case kClientIdle:
+      command_sema_.wait();
+      break;
+
+    case kClientSending:
+      break;
+
+    case kClientReceiving:
+      break;
+  }
+}
+
+void CVSocketClient::run() {
+  while (true) {
+    StateMachine();
+  }
 }
